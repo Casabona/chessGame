618a39a822b4d9959213303d4e63d6be61342160
Casabona Jonathan <casabona.jonathan@gmail.com>
2019-12-04 23:17:16 -0500

    shot logShow init

--- src/main/java/SJCE/more/Log/LogShow.java
+++ src/main/java/SJCE/more/Log/LogShow.java
@@ -12,25 +12,103 @@ public class LogShow extends javax.swing.JFrame {
         this.setTitle("Game Log");
         this.setIconImage(frame.FrameIcon.getImage());        
     }
+private void initButton() {
+	  jToolBar2 = new javax.swing.JToolBar();
+      jToolBar1 = new javax.swing.JToolBar();
+      jButton1 = new javax.swing.JButton();
+      jButton2 = new javax.swing.JButton();
+      jButton3 = new javax.swing.JButton();
+      jButton4 = new javax.swing.JButton();
+      jScrollPane1 = new javax.swing.JScrollPane();
+      gameLogTextArea = new javax.swing.JTextArea();
+      jMenuBar1 = new javax.swing.JMenuBar();
+      jMenu1 = new javax.swing.JMenu();
+      jMenuItem2 = new javax.swing.JMenuItem();
+      jMenuItem4 = new javax.swing.JMenuItem();
+      jMenuItem1 = new javax.swing.JMenuItem();
+      jMenuItem3 = new javax.swing.JMenuItem();
+	
+}
 
+private void setButton(javax.swing.JButton button) {
+	button.setFocusable(false);
+	button.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+	button.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+	button.addActionListener(new java.awt.event.ActionListener() {
+          public void actionPerformed(java.awt.event.ActionEvent evt) {
+              jButton1ActionPerformed(evt);
+          }
+      });
+}
+
+private void initJtoolBar1() {
+    jToolBar1.setFloatable(false);
+
+    jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/floppy_disk_green.png"))); // NOI18N
+    setButton(jButton1);
+    jToolBar1.add(jButton1);
+
+    jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/copy.png"))); // NOI18N
+    jButton2.setToolTipText("Copy Log to ClipBoard");
+    setButton(jButton2);
+    jToolBar1.add(jButton2);
+
+    jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/clipboard_search.png"))); // NOI18N
+    jButton3.setToolTipText("Show ClipBoard");
+    setButton(jButton3);
+    jToolBar1.add(jButton3);
+
+    jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/Log-Out-24.png"))); // NOI18N
+    jButton4.setToolTipText("Close window");
+    setButton(jButton3);
+    jToolBar1.add(jButton4);
+}
+
+private void initJmenu1() {
+	  jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/Actions-document-open-icon.png"))); // NOI18N
+      jMenu1.setText("File");
+
+      jMenuItem2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/clipboard_plus.png"))); // NOI18N
+      jMenuItem2.setText("Copy Log to ClipBoard");
+      jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+          public void actionPerformed(java.awt.event.ActionEvent evt) {
+              jMenuItem2ActionPerformed(evt);
+          }
+      });
+      jMenu1.add(jMenuItem2);
+
+      jMenuItem4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/clipboard_lupa-16.png"))); // NOI18N
+      jMenuItem4.setText("Show ClipBoard");
+      jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+          public void actionPerformed(java.awt.event.ActionEvent evt) {
+              jMenuItem4ActionPerformed(evt);
+          }
+      });
+      jMenu1.add(jMenuItem4);
+
+      jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/save.png"))); // NOI18N
+      jMenuItem1.setText("Save Log");
+      jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+          public void actionPerformed(java.awt.event.ActionEvent evt) {
+              jMenuItem1ActionPerformed(evt);
+          }
+      });
+      jMenu1.add(jMenuItem1);
+
+      jMenuItem3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/Log-Out-16.png"))); // NOI18N
+      jMenuItem3.setText("Exit");
+      jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+          public void actionPerformed(java.awt.event.ActionEvent evt) {
+              jMenuItem3ActionPerformed(evt);
+          }
+      });
+      jMenu1.add(jMenuItem3);
+}
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jToolBar2 = new javax.swing.JToolBar();
-        jToolBar1 = new javax.swing.JToolBar();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        gameLogTextArea = new javax.swing.JTextArea();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
+    	initButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Game Log");
@@ -38,55 +116,7 @@ public class LogShow extends javax.swing.JFrame {
         jToolBar2.setFloatable(false);
         jToolBar2.setOrientation(javax.swing.SwingConstants.VERTICAL);
 
-        jToolBar1.setFloatable(false);
-
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/floppy_disk_green.png"))); // NOI18N
-        jButton1.setToolTipText("Save Log to file");
-        jButton1.setFocusable(false);
-        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jToolBar1.add(jButton1);
-
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/copy.png"))); // NOI18N
-        jButton2.setToolTipText("Copy Log to ClipBoard");
-        jButton2.setFocusable(false);
-        jButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        jToolBar1.add(jButton2);
-
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/clipboard_search.png"))); // NOI18N
-        jButton3.setToolTipText("Show ClipBoard");
-        jButton3.setFocusable(false);
-        jButton3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton3.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-        jToolBar1.add(jButton3);
-
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/24x24/Log-Out-24.png"))); // NOI18N
-        jButton4.setToolTipText("Close window");
-        jButton4.setFocusable(false);
-        jButton4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton4.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-        jToolBar1.add(jButton4);
+        initJtoolBar1();
 
         jToolBar2.add(jToolBar1);
 
@@ -97,44 +127,7 @@ public class LogShow extends javax.swing.JFrame {
 
         jToolBar2.add(jScrollPane1);
 
-        jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/Actions-document-open-icon.png"))); // NOI18N
-        jMenu1.setText("File");
-
-        jMenuItem2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/clipboard_plus.png"))); // NOI18N
-        jMenuItem2.setText("Copy Log to ClipBoard");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem2);
-
-        jMenuItem4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/clipboard_lupa-16.png"))); // NOI18N
-        jMenuItem4.setText("Show ClipBoard");
-        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem4ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem4);
-
-        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/save.png"))); // NOI18N
-        jMenuItem1.setText("Save Log");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem1);
-
-        jMenuItem3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SJCE/img/16x16/Log-Out-16.png"))); // NOI18N
-        jMenuItem3.setText("Exit");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem3);
+        initJmenu1();
 
         jMenuBar1.add(jMenu1);
 
